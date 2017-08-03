e0665522073666cc34c028e0ef7962293041c3e2
Ignacio R. Morelle <shadowm@wesnoth.org>
2017-08-03 14:13:14 -0400

    wesmere: Initial version of the Wesmere integration extension
    It's not as hacky as Glamdrol's because Wesmere has its own search box
    which is (as of Wesmere 1.1.0-dev) completely adequate for the forums.

---
+++ wesmere/event/main_listener.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * phpBB Wesmere style integration extension for Wesnoth.org
+ *
+ * @copyright (c) 2017 Ignacio R. Morelle
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace wesnoth\wesmere\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Event listener
+ */
+class main_listener implements EventSubscriberInterface
+{
+	static public function getSubscribedEvents()
+	{
+		return array();
+	}
+
+	/** @var \phpbb\controller\helper */
+	protected $helper;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	protected $user;
+
+	protected $config;
+
+	/**
+	 * Constructor
+	 *
+	 * @param \phpbb\controller\helper	$helper		Controller helper object
+	 * @param \phpbb\template\template	$template	Template object
+	 */
+	public function __construct(\phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\user $user)
+	{
+		$this->helper = $helper;
+		$this->template = $template;
+		$this->user = $user;
+
+		$this->user->add_lang_ext('wesnoth/wesmere', 'wesmere');
+	}
+}
